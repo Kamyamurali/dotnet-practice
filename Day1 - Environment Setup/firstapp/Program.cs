787bc81c155e470a50739361b4816a03a426e8d7
Kamya Murali <Kamyamurali2008@gmail.com>
2026-08-31 23:31:48 -0400

    Day1 - firstapp console project
    Day1 - firstapp console project

---
+++ Day1 - Environment Setup/firstapp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("This is my very first project in .Net core, and its easy so far");
+
+string myName = "Kamya";
+string myHobby = "Writing";
+
+Console.WriteLine(myName);
+Console.WriteLine(myHobby);
+
+System.Console.WriteLine("Please enter your name");
+string guestName = Console.ReadLine();
+System.Console.WriteLine("Welcome " + guestName);
