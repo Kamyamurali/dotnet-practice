bdcb13929d21017d95475f11db609f5a26efe6f9
Kamya Murali <Kamyamurali2008@gmail.com>
2026-09-11 02:03:38 -0400

    day 8 bankAPPLICATION

---
+++ Day8/bankappREFLECTION/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+var myapp = Assembly.LoadFile("/Users/kamya/Desktop/dotnet-practice/Day8/bankLIB/bin/Debug/net10.0/bankLIB.dll");
+
+Type[] myClasses = myapp.GetTypes();
+
+for (int i = 0; i < myClasses.Length; i++)
+{
+    Console.WriteLine(i + " : " + myClasses[i].FullName);
+}
+Console.WriteLine("------------------------------------------");
+
+Type myClass = myapp.GetType("bankLIB.MyMaths");
+
+var obj = Activator.CreateInstance(myClass);
+
+MethodInfo m = myClass.GetMethod("Add");
+
+object[] parameters = new object[] { 10, 20 };
+
+object result = m.Invoke(obj, parameters);
+
+Console.WriteLine("Add(10, 20) = " + result);
